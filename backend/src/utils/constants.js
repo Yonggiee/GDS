ebcc5f14a30b34fab432cd39f5d0ec78b5dfa24f
Yonggiee <fongyongjie27@gmail.com>
2021-08-02 15:55:42 +0800

    fix overwrap and change domain name

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -2,7 +2,7 @@ const ADJUSTVALUE = 33;
 const BASE10 = 10;
 const BASE62 = 62;
 const BASE62ENCODING = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-const SHORTYURL = 'shorty.tc/';
+const SHORTYURL = 'littleshorty.link/';
 
 module.exports = {
     ADJUSTVALUE,
