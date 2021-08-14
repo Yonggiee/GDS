b70df17792a11168caca5a90a2d4c0d86e66a30c
Yonggiee <fongyongjie27@gmail.com>
2021-08-14 12:32:39 +0800

    use dotenv for local testing without docker

--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -1,9 +1,9 @@
-const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT } = require('../utils/constants');
+const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = require('../utils/constants');
 const { Pool } = require('pg');
 const pool = new Pool({
   user: DB_USER,
   host: DB_HOST,
-  database: DB_DATABASE,
+  database: DB_NAME,
   password: DB_PASSWORD,
   port: DB_PORT
 })
--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,16 +1,16 @@
 const BASE62 = 62;
 const BASE62ENCODING = 'qwert1yu5iopasdfg2hj7k3lzxcv84bnmQWER6TYUI9OPASDFGHJ0KLZXCVBNM';
-const DB_DATABASE = process.env.DATABASE;
-const DB_HOST = process.env.HOST;
-const DB_PASSWORD = process.env.PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
-const DB_USER = process.env.USER;
+const DB_USER = process.env.DB_USER;
 const SHORTYURL = process.env.DNS;
 
 module.exports = {
     BASE62,
     BASE62ENCODING,
-    DB_DATABASE,
+    DB_NAME,
     DB_HOST,
     DB_PASSWORD,
     DB_PORT,
